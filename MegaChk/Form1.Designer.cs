6e39e154bdbf7d00ea4e1ab654081e853a2c0ba3
Risinget <tenorioquirozeduar7@gmail.com>
2026-04-23 21:05:12 -0400

    files

---
+++ MegaChk/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿using System.Windows.Forms;
+
+namespace MegaChk
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.labelLista = new System.Windows.Forms.Label();
+            this.labelSalida = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxLista = new System.Windows.Forms.TextBox();
+            this.textBoxSalida = new System.Windows.Forms.TextBox();
+            this.buttonImprimirLista = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelLista
+            // 
+            this.labelLista.AutoSize = true;
+            this.labelLista.ForeColor = System.Drawing.Color.Red;
+            this.labelLista.Location = new System.Drawing.Point(44, 57);
+            this.labelLista.Name = "labelLista";
+            this.labelLista.Size = new System.Drawing.Size(37, 13);
+            this.labelLista.TabIndex = 0;
+            this.labelLista.Text = "LISTA";
+            // 
+            // labelSalida
+            // 
+            this.labelSalida.AutoSize = true;
+            this.labelSalida.ForeColor = System.Drawing.Color.Red;
+            this.labelSalida.Location = new System.Drawing.Point(393, 57);
+            this.labelSalida.Name = "labelSalida";
+            this.labelSalida.Size = new System.Drawing.Size(45, 13);
+            this.labelSalida.TabIndex = 0;
+            this.labelSalida.Text = "SALIDA";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(396, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "INICIAR VERIFICACIÓN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxLista
+            // 
+            this.textBoxLista.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBoxLista.Location = new System.Drawing.Point(47, 73);
+            this.textBoxLista.Multiline = true;
+            this.textBoxLista.Name = "textBoxLista";
+            this.textBoxLista.Size = new System.Drawing.Size(319, 120);
+            this.textBoxLista.TabIndex = 2;
+            // 
+            // textBoxSalida
+            // 
+            this.textBoxSalida.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBoxSalida.Location = new System.Drawing.Point(396, 73);
+            this.textBoxSalida.Multiline = true;
+            this.textBoxSalida.Name = "textBoxSalida";
+            this.textBoxSalida.Size = new System.Drawing.Size(319, 120);
+            this.textBoxSalida.TabIndex = 2;
+            // 
+            // buttonImprimirLista
+            // 
+            this.buttonImprimirLista.BackColor = System.Drawing.SystemColors.ControlText;
+            this.buttonImprimirLista.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.buttonImprimirLista.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonImprimirLista.ForeColor = System.Drawing.Color.Red;
+            this.buttonImprimirLista.Location = new System.Drawing.Point(214, 22);
+            this.buttonImprimirLista.Name = "buttonImprimirLista";
+            this.buttonImprimirLista.Size = new System.Drawing.Size(152, 23);
+            this.buttonImprimirLista.TabIndex = 1;
+            this.buttonImprimirLista.Text = "IMPRIMIR LISTA";
+            this.buttonImprimirLista.UseVisualStyleBackColor = false;
+            this.buttonImprimirLista.Click += new System.EventHandler(this.buttonImprimirLista_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(563, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "IMPRIMIR SALIDA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeight = 25;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column5});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.Red;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 262);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Red;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.Black;
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(965, 310);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column4
+            // 
+            this.Column4.FillWeight = 50.76142F;
+            this.Column4.HeaderText = "#";
+            this.Column4.Name = "Column4";
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 112.3096F;
+            this.Column1.HeaderText = "ACCOUNT";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 112.3096F;
+            this.Column2.HeaderText = "PASSWORD";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 112.3096F;
+            this.Column3.HeaderText = "USED";
+            this.Column3.Name = "Column3";
+            // 
+            // Column5
+            // 
+            this.Column5.FillWeight = 112.3096F;
+            this.Column5.HeaderText = "QUOTA";
+            this.Column5.Name = "Column5";
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.IpAddress;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(965, 572);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxSalida);
+            this.Controls.Add(this.textBoxLista);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonImprimirLista);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelSalida);
+            this.Controls.Add(this.labelLista);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLista;
+        private System.Windows.Forms.Label labelSalida;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxLista;
+        private System.Windows.Forms.TextBox textBoxSalida;
+        private System.Windows.Forms.Button buttonImprimirLista;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ MegaChk/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CG.Web.MegaApiClient;
+
+namespace MegaChk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonLogin_Click(object sender, EventArgs e)
+        {
+
+            var client = new MegaApiClient();
+            client.Login("asd@gmail.com", "asd");
+
+            foreach (var node in client.GetNodes())
+            {
+                Console.WriteLine($"{node.Type} - {node.Name}");
+            }
+
+
+            var info = client.GetAccountInformation().UsedQuota;
+            Console.WriteLine(info);
+            client.Logout();
+        }
+
+        private void buttonImprimirLista_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(this.textBoxLista.Text);
+
+            String contentLista = this.textBoxLista.Text;
+            string[] lineas = contentLista.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            int initCount = 0;
+            foreach (var line in lineas ) {
+
+                String[] acc = line.Split(new[] { ":" }, StringSplitOptions.None);
+
+                try
+                {
+                    String textConsole = "SOY LA LINEA -> :" + line + " ACCOUNT_1: " + acc[0] + " ACCOUNT_2: " + acc[1];
+                    this.dataGridView1.Rows.Add(initCount++, acc[0], acc[1], "AQUI USED", "AQUI QUOTA");    
+                    Console.WriteLine(textConsole);
+                    this.textBoxSalida.Text = textConsole;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            this.textBoxSalida.Text = this.textBoxLista.Text;
+
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("HOLA");
+            // exportar a .txt todo lo que hay en el datagridview
+                
+        }
+
+        private void buttonExportarTxt_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("asdsadsadsa");
+            // exportar a .txt todo lo que hay en el datagridview
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
